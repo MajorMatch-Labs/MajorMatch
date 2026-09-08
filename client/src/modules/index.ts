982fa2fc0d968d9dbdc365fd33241a681d0a7dc6
NhatPrv <torikun2005@gmail.com>
2026-09-08 13:28:13 +0700

    feat(client): restructure client architecture into 3 feature-driven modules

---
+++ client/src/modules/index.ts
@@ -0,0 +1,11 @@
+/**
+ * BỘ ĐIỀU PHỐI MODULE HÓA KIẾN TRÚC CLIENT - MAJORMATCH
+ * Phân công 3 thành viên theo chuẩn đề cương CS2028 (Chuyên đề 4):
+ * - Module Ingestion: VĂN HOÀNG
+ * - Module Analytics: ÁNH VY
+ * - Module Advisor: LONG NHẬT (Lead)
+ */
+
+export * from "./ingestion";
+export * from "./analytics";
+export * from "./advisor";
